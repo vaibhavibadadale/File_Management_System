d5aa9d61bd9febb0b4802329a6cb7f4e4fa104be
gaurikadam16 <gaurika1608@gmailcom>
2026-04-21 12:35:47 +0530

    update server file

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"], 
+    origin: 'https://filemanagement-h3n7z6186-aaryans-files-projects.vercel.app/',
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], 
     credentials: true,
   })
